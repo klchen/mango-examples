c996c343f26c40e5a0b1284f73d2861ec3561513
Jukka Liimatta <jukka.liimatta@virelabs.com>
2019-07-31 16:43:59 +0300

    Upgrade benchmark to use memory buffer for every test.

--- png_benchmark/benchmark.cpp
+++ png_benchmark/benchmark.cpp
@@ -1,110 +1,98 @@
 /*
     MANGO Multimedia Development Platform
-    Copyright (C) 2012-2018 Twilight Finland 3D Oy Ltd. All rights reserved.
+    Copyright (C) 2012-2019 Twilight Finland 3D Oy Ltd. All rights reserved.
 */
 #include <mango/mango.hpp>
 
 using namespace mango;
 using namespace mango::filesystem;
 
+#define ENABLE_LIBPNG
+#define ENABLE_LODEPNG
 //#define ENABLE_SPNG
-
-// ----------------------------------------------------------------------
-// warmup()
-// ----------------------------------------------------------------------
-
-void warmup(const char* filename)
-{
-    File file(filename);
-    Memory memory = file;
-    std::vector<char> buffer(memory.size);
-    std::memcpy(buffer.data(), memory.address, memory.size);
-}
+#define ENABLE_STB
+#define ENABLE_MANGO
 
 // ----------------------------------------------------------------------
 // libpng
 // ----------------------------------------------------------------------
 
+#if defined ENABLE_LIBPNG
+
 #include <png.h>
 
-void libpng_load(const char* file_name)
+struct png_source
 {
-    int x, y;
+    u8* data;
+    int size;
+    int offset;
+};
 
-    int width, height;
-    png_byte color_type;
-    png_byte bit_depth;
-
-    png_structp png_ptr;
-    png_infop info_ptr;
-    int number_of_passes;
-    png_bytep * row_pointers;
-
-        char header[8];    // 8 is the maximum size that can be checked
-
-        /* open file and test for it being a png */
-        FILE *fp = fopen(file_name, "rb");
-        //if (!fp)
-        //        abort_("[read_png_file] File %s could not be opened for reading", file_name);
-        size_t xx = fread(header, 1, 8, fp);
-        (void) xx;
-        //if (png_sig_cmp(header, 0, 8))
-        //        abort_("[read_png_file] File %s is not recognized as a PNG file", file_name);
-
-
-        /* initialize stuff */
-        png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-
-        //if (!png_ptr)
-        //        abort_("[read_png_file] png_create_read_struct failed");
-
-        info_ptr = png_create_info_struct(png_ptr);
-        //if (!info_ptr)
-        //        abort_("[read_png_file] png_create_info_struct failed");
-
-        //if (setjmp(png_jmpbuf(png_ptr)))
-        //        abort_("[read_png_file] Error during init_io");
-
-        png_init_io(png_ptr, fp);
-        png_set_sig_bytes(png_ptr, 8);
-
-        png_read_info(png_ptr, info_ptr);
-
-        width = png_get_image_width(png_ptr, info_ptr);
-        height = png_get_image_height(png_ptr, info_ptr);
-        color_type = png_get_color_type(png_ptr, info_ptr);
-        bit_depth = png_get_bit_depth(png_ptr, info_ptr);
-
-        number_of_passes = png_set_interlace_handling(png_ptr);
-        png_read_update_info(png_ptr, info_ptr);
-
-
-        /* read file */
-        //if (setjmp(png_jmpbuf(png_ptr)))
-        //        abort_("[read_png_file] Error during read_image");
-
-        row_pointers = (png_bytep*) malloc(sizeof(png_bytep) * height);
-        for (y=0; y<height; y++)
-                row_pointers[y] = (png_byte*) malloc(png_get_rowbytes(png_ptr,info_ptr));
-
-        png_read_image(png_ptr, row_pointers);
-
-        fclose(fp);
+static void png_read_callback(png_structp png_ptr, png_bytep data, png_size_t length)
+{
+    png_source* source = (png_source*) png_get_io_ptr(png_ptr);
+    std::memcpy(data, source->data + source->offset, length);
+    source->offset += length;
 }
+
+void load_libpng(Memory memory)
+{
+    png_structp png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+
+    png_infop info_ptr = png_create_info_struct(png_ptr);
+
+    png_source source;
+    source.data = memory.address + 8;
+    source.size = memory.size;
+    source.offset = 0;
+    png_set_read_fn(png_ptr, &source, png_read_callback);
+
+    png_set_sig_bytes(png_ptr, 8);
+    png_read_info(png_ptr, info_ptr);
+
+    int width = png_get_image_width(png_ptr, info_ptr);
+    int height = png_get_image_height(png_ptr, info_ptr);
+    png_byte color_type = png_get_color_type(png_ptr, info_ptr);
+    png_byte bit_depth = png_get_bit_depth(png_ptr, info_ptr);
+
+    int number_of_passes = png_set_interlace_handling(png_ptr);
+    png_read_update_info(png_ptr, info_ptr);
+
+    int stride = png_get_rowbytes(png_ptr, info_ptr);
+    u8* image = (u8*)malloc(stride * height);
+    png_bytep *row_pointers = (png_bytep*) malloc(sizeof(png_bytep) * height);
+
+
+    for (int y=0; y<height; y++)
+    {
+        row_pointers[y] = image + stride * y;
+    }
+
+    png_read_image(png_ptr, row_pointers);
+
+    free(row_pointers);
+    free(image);
+}
+
+#endif
 
 // ----------------------------------------------------------------------
 // lodepng
 // ----------------------------------------------------------------------
 
+#if defined ENABLE_LODEPNG
+
 #include "lodepng/lodepng.h"
 
-void lode_decodeOneStep(const char* filename)
+void load_lodepng(Memory memory)
 {
-  std::vector<unsigned char> image;
-  unsigned width, height;
-  unsigned error = lodepng::decode(image, width, height, filename);
-  //if(error) std::cout << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
+    u32 width, height;
+    u8* image;
+    int error = lodepng_decode32(&image, &width, &height, memory.address, memory.size);
+    free(image);
 }
+
+#endif
 
 // ----------------------------------------------------------------------
 // spng
@@ -116,134 +104,155 @@ void lode_decodeOneStep(const char* filename)
 
 #include <spng.h>
 
-Surface load_spng(const char* filename)
+struct read_fn_state
 {
-    Surface s(0, 0, Format(), 0, nullptr);
+    unsigned char *data;
+    size_t bytes_left;
+};
 
-    FILE *png;
-    char *pngbuf;
-    png = fopen(filename, "r");
+int read_fn(struct spng_ctx *ctx, void *user, void *data, size_t n)
+{
+    struct read_fn_state *state = (struct read_fn_state *) user;
+    if(n > state->bytes_left) return SPNG_IO_EOF;
 
-    if(png==NULL)
-    {
-        printf("error opening input file %s\n", filename);
-        return s;
-    }
+    unsigned char *dst = (u8*)data;
+    unsigned char *src = state->data;
 
-    fseek(png, 0, SEEK_END);
+#if defined(TEST_SPNG_STREAM_READ_INFO)
+    printf("libspng bytes read: %lu\n", n);
+#endif
 
-    long siz_pngbuf = ftell(png);
-    rewind(png);
+    memcpy(dst, src, n);
 
-    if(siz_pngbuf < 1)
-        return s;
+    state->bytes_left -= n;
+    state->data += n;
 
-    pngbuf = (char*)malloc(siz_pngbuf);
-    if(pngbuf==NULL)
-    {
-        printf("malloc() failed\n");
-        return s;
-    }
+    return 0;
+}
 
-    if(fread(pngbuf, siz_pngbuf, 1, png) != 1)
-    {
-        printf("fread() failed\n");
-        return s;
-    }
+unsigned char *getimage_libspng(unsigned char *buf, size_t size, size_t *out_size, int fmt, int flags, struct spng_ihdr *info)
+{
+    int r;
+    size_t siz;
+    unsigned char *out = NULL;
+    struct spng_ihdr ihdr;
 
     spng_ctx *ctx = spng_ctx_new(0);
-    if(ctx == NULL)
+
+    if(ctx==NULL)
     {
         printf("spng_ctx_new() failed\n");
-        return s;
+        return NULL;
     }
 
-    int r;
-    r = spng_set_png_buffer(ctx, pngbuf, siz_pngbuf);
+    /*struct read_fn_state state;
+    state.data = buf;
+    state.bytes_left = size;
+    r = spng_set_png_stream(ctx, read_fn, &state);
+    if(r)
+    {
+        printf("spng_set_png_stream() error: %s\n", spng_strerror(r));
+        goto err;
+    }*/
+
+
+    spng_set_crc_action(ctx, SPNG_CRC_USE, SPNG_CRC_USE);
+
+    r = spng_set_png_buffer(ctx, buf, size);
 
     if(r)
     {
         printf("spng_set_png_buffer() error: %s\n", spng_strerror(r));
-        return s;
+        goto err;
     }
 
-    struct spng_ihdr ihdr;
     r = spng_get_ihdr(ctx, &ihdr);
 
     if(r)
     {
         printf("spng_get_ihdr() error: %s\n", spng_strerror(r));
-        return s;
+        goto err;
     }
 
-#if 0
-    const char *clr_type_str;
-    if(ihdr.color_type == SPNG_COLOR_TYPE_GRAYSCALE)
-        clr_type_str = "grayscale";
-    else if(ihdr.color_type == SPNG_COLOR_TYPE_TRUECOLOR)
-        clr_type_str = "truecolor";
-    else if(ihdr.color_type == SPNG_COLOR_TYPE_INDEXED)
-        clr_type_str = "indexed color";
-    else if(ihdr.color_type == SPNG_COLOR_TYPE_GRAYSCALE_ALPHA)
-        clr_type_str = "grayscale with alpha";
-    else
-        clr_type_str = "truecolor with alpha";
+    memcpy(info, &ihdr, sizeof(struct spng_ihdr));
 
-    printf("width: %" PRIu32 "\nheight: %" PRIu32 "\n"
-           "bit depth: %" PRIu8 "\ncolor type: %" PRIu8 " - %s\n",
-           ihdr.width, ihdr.height,
-           ihdr.bit_depth, ihdr.color_type, clr_type_str);
-    printf("compression method: %" PRIu8 "\nfilter method: %" PRIu8 "\n"
-           "interlace method: %" PRIu8 "\n",
-           ihdr.compression_method, ihdr.filter_method,
-           ihdr.interlace_method);
-#endif
+    r = spng_decoded_image_size(ctx, fmt, &siz);
+    if(r) goto err;
 
-    size_t out_size;
+    *out_size = siz;
 
-    r = spng_decoded_image_size(ctx, SPNG_FMT_RGBA8, &out_size);
+    out = (unsigned char*)malloc(siz);
+    if(out==NULL) goto err;
 
-    if(r)
-        return s;
-
-    unsigned char *out = (u8*) malloc(out_size);
-    if(out==NULL)
-        return s;
-
-    r = spng_decode_image(ctx, out, out_size, SPNG_FMT_RGBA8, 0);
+    r = spng_decode_image(ctx, out, siz,  fmt, flags);
 
     if(r)
     {
         printf("spng_decode_image() error: %s\n", spng_strerror(r));
-        return s;
+        goto err;
     }
 
     spng_ctx_free(ctx);
 
-    /* write raw pixels to file */
-    //fwrite(out, out_size, 1, raw);
+goto skip_err;
 
-    free(out);
-    free(pngbuf);
-    return s;
+err:
+    spng_ctx_free(ctx);
+    if(out !=NULL) free(out);
+    return NULL;
+
+skip_err:
+
+    return out;
 }
 
-#endif // ENABLE_SPNG
+void load_spng(Memory memory)
+{
+    struct spng_ihdr ihdr;
+    size_t img_spng_size;
+
+    u8* image = getimage_libspng(memory.address, memory.size, &img_spng_size, SPNG_FMT_RGBA8, 0, &ihdr);
+    free(image);
+}
+
+#endif
 
 // ----------------------------------------------------------------------
 // stb
 // ----------------------------------------------------------------------
 
+#if defined ENABLE_STB
+
 #define STB_IMAGE_IMPLEMENTATION
 #include "../jpeg_benchmark/stb_image.h"
 
-Surface stb_load_png(const char* filename)
+void load_stb(Memory memory)
 {
     int width, height, bpp;
-    u8* rgb = stbi_load(filename, &width, &height, &bpp, 3);
- 
-    return Surface(width, height, FORMAT_R8G8B8, width * 3, rgb);
+    u8* image = stbi_load_from_memory(memory.address, memory.size, &width, &height, &bpp, 4);
+    free(image);
 }
+
+#endif
+
+// ----------------------------------------------------------------------
+// mango
+// ----------------------------------------------------------------------
+
+#if defined(ENABLE_MANGO)
+
+void load_mango(Memory memory)
+{
+    ImageDecoder decoder(memory, ".png");
+
+    ImageHeader header = decoder.header();
+    Bitmap bitmap(header.width, header.height, header.format);
+
+    ImageDecodeOptions options;
+    decoder.decode(bitmap, options);
+}
+
+#endif
 
 // ----------------------------------------------------------------------
 // main()
@@ -258,83 +267,87 @@ int main(int argc, const char* argv[])
     }
 
     const char* filename = argv[1];
-    warmup(filename);
+
+    File file(filename);
+    Buffer buffer(file);
 
     u64 time0;
     u64 time1;
 
     // ------------------------------------------------------------------
 
+#if defined ENABLE_LIBPNG
+
     printf("load libpng:  ");
     time0 = Time::us();
 
-    libpng_load(filename);
+    load_libpng(buffer);
 
     time1 = Time::us();
     printf("%5d.%d ms\n", int((time1 - time0)/1000), int((time1 - time0)%1000));
 
+#endif
+
     // ------------------------------------------------------------------
+
+#if defined ENABLE_LODEPNG
 
     printf("load lodepng: ");
     time0 = Time::us();
 
-    lode_decodeOneStep(filename);
+    load_lodepng(buffer);
 
     time1 = Time::us();
     printf("%5d.%d ms\n", int((time1 - time0)/1000), int((time1 - time0)%1000));
 
+#endif
+
     // ------------------------------------------------------------------
+
 #if defined(ENABLE_SPNG)
+
     printf("load spng:    ");
     time0 = Time::us();
 
-    Surface s = load_spng(filename);
+    load_spng(buffer);
 
     time1 = Time::us();
     printf("%5d.%d ms\n", int((time1 - time0)/1000), int((time1 - time0)%1000));
+
 #endif
+
     // ------------------------------------------------------------------
+
+#if defined(ENABLE_STB)
 
     printf("load stb:     ");
     time0 = Time::us();
 
-    Surface s_stb = stb_load_png(filename);
+    load_stb(buffer);
 
     time1 = Time::us();
     printf("%5d.%d ms\n", int((time1 - time0)/1000), int((time1 - time0)%1000));
 
+#endif
+
     // ------------------------------------------------------------------
+
+#if defined(ENABLE_MANGO)
 
     printf("load mango:   ");
     time0 = Time::us();
 
-#if 0
-    Bitmap bitmap(filename);
-#else
-    FileStream file(filename, Stream::READ);
-    Buffer buffer(file.size());
-    file.read(buffer.data(), file.size());
-
-    ImageDecoder decoder(buffer, filename);
-    ImageHeader header = decoder.header();
-
-    Bitmap bitmap(header.width, header.height, header.format);
-    //Bitmap bitmap(header.width, header.height, IndexedFormat(8));
-    //Palette palette;
-
-    ImageDecodeOptions options;
-    //options.palette = &palette;
-
-    decoder.decode(bitmap, options);
-#endif
+    load_mango(buffer);
 
     time1 = Time::us();
     printf("%5d.%d ms\n", int((time1 - time0)/1000), int((time1 - time0)%1000));
 
+#endif
+
     // ------------------------------------------------------------------
 
-    printf("\n");
-    printf("image: %d x %d\n", bitmap.width, bitmap.height);
+    //printf("\n");
+    //printf("image: %d x %d\n", bitmap.width, bitmap.height);
 
     //bitmap.save("/test/debug.png");
     //s_stb.save("debug.jpg");
